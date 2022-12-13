7b32db13a47d0483ebf8124b34e502f55bf1fe4e
MoonJinjin <jinjin.moon@monitorapp.com>
2022-12-13 10:55:12 +0900

    set background image

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var bg_ul_div = document.getElementById("bg_ul_div");
+var main_canvas = document.getElementById("main_canvas");
 
 var bg_prev = 1;
 document.addEventListener("DOMContentLoaded", function () {
   makeBgImgArray();
-  setBgImg(1);
+  setBgImg();
 });
 
 var bg_img_arr = [];
@@ -13,19 +14,23 @@ function makeBgImgArray() {
   }
 }
 
-function setBgImg(index) {
+function setBgImg() {
   var elem_bg_ul = document.createElement('ul');
   elem_bg_ul.setAttribute("id", "bg_ul");
   elem_bg_ul.style.padding = 0;
 
-  for (var i = index; i < bg_img_arr.length; i++) {
+  for (var i = 0; i < bg_img_arr.length; i++) {
     var bg_li = document.createElement('li');
     bg_li.style.listStyle = "none";
+    bg_li.classList.add("bg_li");
 
     var bg_img = document.createElement('img');
     bg_img.src = bg_img_arr[i];
     bg_img.width = 100;
     bg_img.height = 100;
+    bg_img.addEventListener("click", function(e) {
+      main_canvas.style.backgroundImage = `url('${e.target.src}')`;
+    });
     bg_li.appendChild(bg_img);
     elem_bg_ul.appendChild(bg_li);
   }
@@ -34,19 +39,12 @@ function setBgImg(index) {
 
 var bg_prev_btn = document.getElementById("bg_prev_btn");
 bg_prev_btn.addEventListener("click", function () {
-  if (bg_prev !== 1) {
-    --bg_prev;
-  }
-  document.getElementById("bg_ul").remove();
-  setBgImg(bg_prev);
+  var bg_li = document.getElementsByClassName("bg_li");
+  bg_ul.insertBefore(bg_li[bg_li.length-1], bg_li[0]);
 })
 
 var bg_next_btn = document.getElementById("bg_next_btn");
 bg_next_btn.addEventListener("click", function () {
-  if (bg_prev == 10) {
-    bg_prev = 1;
-  }
-  ++bg_prev;
-  document.getElementById("bg_ul").remove();
-  setBgImg(bg_prev);
+  var bg_li = document.getElementsByClassName("bg_li");
+  bg_ul.insertBefore(bg_li[0], null);
 })
